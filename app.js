26755592b12a25126ffa2f859ce122eaf3aa199c
WonderXuy <veko.dmitriy0@gmail.com>
2020-03-26 10:56:41 +0300

    LogisticsAgency/init: init project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = require('./routers/export-router');
+
+
+const app = express();
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3020;
+
+app.use(express.json());
+app.use(express.static(__dirname + '/pages'));
+//app.use('/', router.pageRouter);
+app.use('/users', router.userRouter);
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
