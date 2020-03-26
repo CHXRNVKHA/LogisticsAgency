bbcc089b59a5730606a8e78f6b2fc534449d9a02
WonderXuy <veko.dmitriy0@gmail.com>
2020-03-26 11:02:46 +0300

    LogisticsAgency/feat: add router for orders

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3020;
 app.use(express.json());
 app.use(express.static(__dirname + '/pages'));
 //app.use('/', router.pageRouter);
-app.use('/users', router.userRouter);
+app.use('/users', router.orderRouter);
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
