858ab5dcfb1107ab01aa4b9a8e905f2def0bc97d
WonderXuy <veko.dmitriy0@gmail.com>
2020-03-26 11:45:04 +0300

    LogisticsAgency/fix: change contact controller functions

--- controllers/contract-controller.js
+++ controllers/contract-controller.js
@@ -1,58 +1,15 @@
 const service = require('../services/contract-service');
 class ContractController {
     constructor(){};
-    saveChanges = async (req, res) => {
+    addContract = async (req, res) => {
         try {
-            const result = await service.saveChanges();
+            const result = await service.add(req.body.name, req.body.age);
             res.send(result);
         }
         catch (e) {
             res.status(400).send({error:e.message});
         }
-    }
-    deleteContract = async (req, res) => {
-        try {
-            const result = await service.deleteContract(req.body.id);
-            res.send(result);
-        }
-        catch (e) {
-            res.status(400).send({error:e.message});
-        }
-    }
-    updateContract = async (req, res) => {
-        try {
-            const result = await service.updateContract(req.body.id, req.body.name, req.body.age);
-            res.send(result);
-        }
-        catch (e) {
-            res.status(400).send({error:e.message});
-        }
-    }
-    createContract = async (req, res) => {
-        try {
-            const result = await service.createContract(req.body.name, req.body.age);
-            res.send(result);
-        }
-        catch (e) {
-            res.status(400).send({error:e.message});
-        }
-    }
-    getContracts = async (req, res) => {
-        try {
-            const result = await service.getContracts();
-            res.send(result);
-        } catch (e) {
-            res.status(400).send({error:e.message});
-        }
-    }
-    getContractById = async (req, res) => {
-        try {
-            const result = await service.getContractById(req.params.id);
-            res.send(result);
-        } catch (e) {
-            res.status(400).send({error:e.message});
-        }
-    }
+    };
 }
 
 module.exports = ContractController;
