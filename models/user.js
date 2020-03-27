a8b71deb282293a2e17361e709e00e96f9c56bb6
WonderXuy <veko.dmitriy0@gmail.com>
2020-03-27 11:39:39 +0300

    logisticsAgency/models/feat: add pre save hook for user

--- models/user.js
+++ models/user.js
@@ -60,3 +60,11 @@ user.methods.generateAuthToken = async function () {
     await user.save();
     return token;
 };
+
+user.pre('save', async function(next){
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next();
+})
