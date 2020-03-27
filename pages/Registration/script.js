ad4cf28556c5221e3da8a47fc1950de5c1a87618
WonderXuy <veko.dmitriy0@gmail.com>
2020-03-27 17:02:55 +0300

    logisticsagency/routers/feat: add user router

--- pages/Registration/script.js
+++ pages/Registration/script.js
@@ -1,8 +1,36 @@
 const form = document.querySelector('#form');
 
 const mainListener = function(e) {
-    event.preventDefault();
-    console.log(e.target);
+  event.preventDefault();
+  switch (e.target.name) {
+    case 'submit':
+      break;
+
+    default:
+      break;
+  }
+};
+
+const createUser = async function(e) {
+  event.preventDefault();
+  const userName = this.elements['name'].value;
+  const userSurname = this.elements['surname'].value;
+  const userLogin = this.elements['login'].value;
+  const userPassword = this.elements['password'].value;
+  const response = await fetch('/user/add', {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify({
+      name: userName,
+      surname: userSurname,
+      login: userLogin,
+      password: userPassword,
+    })
+  });
+  const user = await response.json();
 };
 
 form.addEventListener('click', mainListener);
---
+++ routers/user-router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const UserController = require('../controllers/user-controller')
+const userController = new UserController();
+
+const router = new express.Router();
+
+//router.get('/', userController.getAllusers);
+router.post('/add', userController.addUser);
+//router.put('/update', userController.updateuser);
+//router.delete('/delete/:id', userController.deleteuser);
+
+module.exports = router;
