6a2cb108e153083695bb19a033d2901bd2dfbf9b
WonderXuy <veko.dmitriy0@gmail.com>
2020-03-31 11:44:48 +0300

    logisticsagency/middleware: add verify for sign up

---
+++ middleware/verifySignUp.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+const checkDuplicateUsernameOrEmail = async (req, res, next) => {
+    const user = await User.findOne({
+      where: {
+        username: req.body.name
+      }
+    });
+    if (user) {
+        return res.status(400).send({
+            message: 'Failed! Username is already in use!'
+        })
+    }
+    const user = await User.findOne({
+        where: {
+          email: req.body.email
+        }
+    });
+    if (user) {
+        return res.status(400).send({
+            message: "Failed! Email is already in use!"
+        });
+    }
+    next();
+};
+
+module.exports = {
+    checkDuplicateUsernameOrEmail,
+}
--- routers/user-router.js
+++ routers/user-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const auth = require('../middleware/auth');
+const verifySignUp = require('../middleware/verifySignUp');
 
 const UserController = require('../controllers/user-controller')
 const userController = new UserController();
@@ -7,7 +8,7 @@ const userController = new UserController();
 const router = new express.Router();
 
 //router.get('/', userController.getAllusers);
-router.post('/add', userController.addUser);
+router.post('/add', verifySignUp.checkDuplicateUsernameOrEmail, userController.addUser);
 router.post('/login', userController.loginUser);
 //router.delete('/delete/:id', userController.deleteuser);
 
