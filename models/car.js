95c0d2fd023d1d1996845eebfc358f766e070027
Veko Dmitry <vekodmitriy0@gmail.com>
2020-04-10 16:24:22 +0300

    logisticsAgency/models/feat: add car model

---
+++ models/car.js
@@ -0,0 +1,29 @@
+const conn = require('./connection');
+const { DataTypes } = require('sequelize');
+const carStatus = require('./carStatus');
+const carRegistyInf = require('./carRegistryInf');
+
+const Car = conn.define('car', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    mark: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+}, {
+  tableName: 'Car',
+});
+
+Car.hasOne(carStatus, { onDelete: 'cascade', onUpdate: 'cascade' });
+Car.hasOne(carRegistyInf, { onDelete: 'cascade', onUpdate: 'cascade' });
+ 
+
+module.exports = Car
--- models/contract.js
+++ models/contract.js
@@ -5,6 +5,7 @@ const contractStatus = require('./contract-status');
 const driver = require('./driver');
 const bill = require('./bill');
 const customer = require('./customer');
+const car = require('./car');
 
 
 const Contract = conn.define('contract', {
@@ -27,5 +28,6 @@ Contract.hasOne(contractStatus, { onDelete: 'cascade', onUpdate: 'cascade' });
 Contract.hasOne(driver, { onDelete: 'cascade', onUpdate: 'cascade' });
 Contract.hasOne(bill, { onDelete: 'cascade', onUpdate: 'cascade' });
 Contract.hasOne(customer, { onDelete: 'cascade', onUpdate: 'cascade' });
+Contract.hasOne(car, { onDelete: 'cascade', onUpdate: 'cascade' });
 
 module.exports = Contract
